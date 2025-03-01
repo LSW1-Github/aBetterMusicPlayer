920c6d5b70c864a23191725b1f559af9a943a0ea
LSW1-Github <145826573+LSW1-Github@users.noreply.github.com>
2025-02-28 23:58:26 -0800

    I rearranged a few things.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import subprocess
 import customtkinter as ctk
 import logging as lg
 import numpy as np
@@ -7,18 +8,20 @@ import musicCon as msCn
 from musicCon import *
 import listing as lst
 from listing import *
-import datetime
+import pygame
+from pygame import *
 
+mus = pygame.mixer
 
-lg.getLogger(__name__)
-log_file = "lgFile.log"
+lg_file = "lgFile.log"
 lg.basicConfig(
-    filename=log_file,
+    filename=lg_file,
     level=lg.DEBUG,
     format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     filemode='w',
     )
+lg = lg.getLogger(__name__)
 
 placement = [
     0,#0
@@ -37,12 +40,11 @@ class main:
         lg.info('APPLICATION STARTED' '\n' '------------------------------------------------------------------------------------------------------------')
         
         try:
+            mus.init(frequency=48000, buffer=1024) 
+            lg.info(f' mus.init = {mus.init}')
             self.boooop = 0.00
             self.sefont = msCn.fonts()
             lg.info(f"{self.sefont} LOADED.")
-
-
-
             ############# FIRST FRAME ####################
             self.firstFrame = ctk.CTkFrame(
                 master=self.root,
@@ -60,10 +62,10 @@ class main:
                 ipadx=5,
                 )
             ############# FIRST FRAME ####################
-
             ############# FIRST FRAME ####################
             self.switch_var = ctk.StringVar(value="on")
-            self.switch = ctk.CTkSwitch(master=self.firstFrame,
+            self.switch = ctk.CTkSwitch(
+                master=self.firstFrame,
                 text="MUTED",
                 width=120,
                 height=100,
@@ -93,12 +95,31 @@ class main:
                 ipady=5,
                 ipadx=5,
                 )
-
-
-
+            
+            self.openLogButton = ctk.CTkButton(
+                master=self.firstFrame,
+                command=self.openLog,
+                corner_radius=lst.crd[0],
+                fg_color="grey",
+                font=self.sefont,
+                text="openLog",
+                text_color = clrs[0],
+                border_color=lst.clrs[8],
+                width = 50,
+                height = 50,
+                border_width = 5,
+                border_spacing = 5,
+                )
+            self.openLogButton.grid(
+                row=placement[0],
+                column=placement[1],
+                padx=5,
+                pady=5,
+                ipady=5,
+                ipadx=5,
+                )
             ############# SECOND FRAME ####################
             ############# SECOND FRAME ####################
-
             self.secondFrame = ctk.CTkFrame(
                 master=self.root,
                 bg_color=lst.clrs[2],
@@ -115,14 +136,11 @@ class main:
                 ipady=5,
                 ipadx=5,
                 )
-
             ############# SECOND FRAME ####################
             ############# SECOND FRAME ####################
-
-
             self.firstTextbox = ctk.CTkTextbox(
                 master = self.secondFrame,
-                width = 500,
+                width = 700,
                 height = 400,
                 corner_radius= crd[0],
                 border_width = 5,
@@ -144,11 +162,8 @@ class main:
                 ipady=5,
                 ipadx=5,
                 )
-
-
             ############# THIRD FRAME ####################
             ############# THIRD FRAME ####################
-
             self.thirdFrame = ctk.CTkFrame(
                 master=self.root,
                 bg_color=lst.clrs[2],
@@ -167,7 +182,6 @@ class main:
                 )
             ############# THIRD FRAME ####################
             ############# THIRD FRAME ####################
-
             self.boopButton = ctk.CTkButton(
                 master=self.thirdFrame,
                 command=self.boop,
@@ -175,7 +189,7 @@ class main:
                 fg_color="grey",
                 font=self.sefont,
                 text="BOOP",
-                text_color = clrs[0],
+                text_color = lst.clrs[0],
                 border_color=lst.clrs[8],
                 width = 50,
                 height = 50,
@@ -190,12 +204,8 @@ class main:
                 ipady=5,
                 ipadx=5,
                 )
-
-
-
             ############# FOURTH FRAME ####################
             ############# FOURTH FRAME ####################
-
             self.fourthFrame = ctk.CTkFrame(
                 master=self.root,
                 bg_color=lst.clrs[2],
@@ -212,12 +222,8 @@ class main:
                 ipady=5,
                 ipadx=5,
                 )
-
-
-
             ############# FOURTH FRAME ####################
             ############# FOURTH FRAME ####################
-
             self.addMultipleSongsButton = ctk.CTkButton(
                 master=self.fourthFrame,
                 command=self.addMultipleSongsBUTT,
@@ -240,7 +246,6 @@ class main:
                 ipadx=5,
                 )
 
-
             self.addSongButton= ctk.CTkButton(
                 master=self.fourthFrame,
                 command=self.addSongBUTT,
@@ -262,13 +267,8 @@ class main:
                 ipady=5,
                 ipadx=5,
                 )
-
-
-
             ############# MUSIC FRAME ####################
             ############# MUSIC FRAME ####################
-
-
             self.musicFrame = ctk.CTkFrame(
                 master=self.root,
                 bg_color=lst.clrs[2],
@@ -284,13 +284,8 @@ class main:
                 ipady=5,
                 ipadx=5,
                 )
-
-
             ############# MUSIC FRAME ####################
             ############# MUSIC FRAME ####################
-
-
-
             self.loadLibraryButton = ctk.CTkButton(
                 master=self.musicFrame,
                 command=self.loadLibraryBUTT,
@@ -312,7 +307,6 @@ class main:
                 ipady=5,
                 ipadx=5,
                 )
-
 
             self.pauseButton = ctk.CTkButton(
                 master=self.musicFrame,
@@ -358,8 +352,6 @@ class main:
                 ipadx=5,
                 )
 
-
-
             self.playButton = ctk.CTkButton(
                 master=self.musicFrame,
                 command=self.playBUTT,
@@ -382,12 +374,204 @@ class main:
                 ipadx=5,
                 )
 
+            self.checkMixerButton = ctk.CTkButton(
+                master=self.musicFrame,
+                command=self.checkMixerStatus,
+                corner_radius=lst.crd[0],
+                font=self.sefont,
+                text_color = clrs[0],
+                border_color=lst.clrs[8],
+                width = 50,
+                height = 50,
+                border_width = 5,
+                border_spacing = 5,
+                text="Mixer Status"
+                )
+            self.checkMixerButton.grid(
+                row=placement[1],
+                column=placement[0],
+                padx=5,
+                pady=5,
+                ipady=5,
+                ipadx=5,
+                )
+                
+
+            self.activateMixerButton = ctk.CTkButton(
+                master=self.musicFrame,
+                command=self.activateMixerStatus,
+                corner_radius=lst.crd[0],
+                font=self.sefont,
+                text_color = clrs[0],
+                border_color=lst.clrs[8],
+                width = 50,
+                height = 50,
+                border_width = 5,
+                border_spacing = 5,
+                text="Activate Mixer"
+                )
+            self.activateMixerButton.grid(
+                row=placement[1],
+                column=placement[1],
+                padx=5,
+                pady=5,
+                ipady=5,
+                ipadx=5,
+                )
+
+
+
+
+            self.shutDownMixerButton = ctk.CTkButton(
+                master=self.musicFrame,
+                command=self.shutDownMixerStatus,
+                corner_radius=lst.crd[0],
+                font=self.sefont,
+                text_color = clrs[0],
+                border_color=lst.clrs[8],
+                width = 50,
+                height = 50,
+                border_width = 5,
+                border_spacing = 5,
+                text="shut Down Mixer"
+                )
+            self.shutDownMixerButton.grid(
+                row=placement[1],
+                column=placement[2],
+                padx=5,
+                pady=5,
+                ipady=5,
+                ipadx=5,
+                )
+
+
+
+
+
+
             ############# MUSIC FRAME ####################
             ############# MUSIC FRAME ####################
+            ############# FIFTH FRAME #####################
+            ############# FIFTH FRAME #####################
+            ############# FIFTH FRAME #####################
 
+            self.fifthFrame = ctk.CTkFrame(
+                master=self.root,
+                width=800,
+                height=600,
+                )
+            self.fifthFrame.grid(
+                row=placement[1],
+                column=placement[1],
+                padx=5,
+                pady=5,
+                ipady=5,
+                ipadx=5,
+                )
+            ############# FIFTH FRAME #####################
+            ############# FIFTH FRAME #####################
+            ############# FIFTH FRAME #####################
+            ############# FIFTH FRAME #####################
 
+            self.artistLabel = ctk.CTkLabel(
+                master=self.fifthFrame,
+                width=20,
+                height=3,
+                text="Artist",
+                bg_color=clrs[1],
+                corner_radius=lst.crd[0],
+                font=self.sefont,
+                text_color = clrs[0],
+                )
+            self.artistLabel.grid(
+                row=placement[0],
+                column=placement[0],
+                padx=5,
+                pady=5,
+                ipady=5,
+                ipadx=5,
+                )
+            
+            self.albumLabel = ctk.CTkLabel(
+                master=self.fifthFrame,
+                width=20,
+                height=3,
+                text="Album",
+                bg_color=clrs[1],
+                corner_radius=lst.crd[0],
+                font=self.sefont,
+                text_color = clrs[0],
+                )
+            self.albumLabel.grid(
+                row=placement[0],
+                column=placement[1],
+                padx=5,
+                pady=5,
+                ipady=5,
+                ipadx=5,
+                )
 
+            self.trackLabel = ctk.CTkLabel(
+                master=self.fifthFrame,
+                width=20,
+                height=3,
+                text="Track",
+                bg_color=clrs[1],
+                corner_radius=lst.crd[0],
+                font=self.sefont,
+                text_color = clrs[0],
+                )
+            self.trackLabel.grid(
+                row=placement[0],
+                column=placement[2],
+                padx=5,
+                pady=5,
+                ipady=5,
+                ipadx=5,
+                )
 
+            self.yearLabel = ctk.CTkLabel(
+                master=self.fifthFrame,
+                width=20,
+                height=3,
+                text="Year",
+                bg_color=clrs[1],
+                corner_radius=lst.crd[0],
+                font=self.sefont,
+                text_color = clrs[0],
+                )
+            self.yearLabel.grid(
+                row=placement[0],
+                column=placement[3],
+                padx=5,
+                pady=5,
+                ipady=5,
+                ipadx=5,
+                )
+
+            self.genreLabel = ctk.CTkLabel(
+                master=self.fifthFrame,
+                width=20,
+                height=3,
+                text="Genre",
+                bg_color=clrs[1],
+                corner_radius=lst.crd[0],
+                font=self.sefont,
+                text_color = clrs[0],
+                )
+            self.genreLabel.grid(
+                row=placement[0],
+                column=placement[4],
+                padx=5,
+                pady=5,
+                ipady=5,
+                ipadx=5,
+                )
+
+            ############# FIFTH FRAME #####################
+            ############# FIFTH FRAME #####################
+            ############# FIFTH FRAME #####################
+            ############# FIFTH FRAME #####################
         except Exception as e:
             lg.exception(f"Error in __init__: {e}")
             lg.info(f"Error in __init__: {e}")
@@ -427,9 +611,9 @@ class main:
             lg.info(f"Error in boop: {e}")
             print(f"Error in boop: {e}")
 
-
     def addSongBUTT(self):
         try:
+            self.firstTextbox.insert("0.1", f'addSong - SONG IS NOW ADDED TO LIBRARY' '\n')
             lg.info(f'addSong - SONG IS NOW ADDED TO LIBRARY')
             self.firstTextbox.insert("0.1", 'addSong - SONG IS NOW ADDED TO LIBRARY,' '\n')
 
@@ -440,23 +624,20 @@ class main:
         try:
             lg.info(f'loadLibraryBUTT - LOAD LIBRARY')
             self.firstTextbox.insert("0.1", 'loadLibraryBUTT - LOAD LIBRARY,' '\n')
-
         except Exception as e:
             lg.exception(f"Error in loadLibraryBUTT: {e}")
 
-
-
     def pauseBUTT(self):
         try:
+            self.firstTextbox.insert("0.1", f'addSong - SONG IS NOW ADDED TO LIBRARY' '\n')
             lg.info(f'pauseBUTT - '"NOW PAUSED")
             self.firstTextbox.insert("0.1", 'pauseBUTT - NOW PAUSE' '\n')
         except Exception as e:
             lg.exception(f'Error in pauseBUTT: {e}')
 
-
-
     def playAlbumBUTT(self):
         try:
+            mus.play()            
             lg.info(f'playAlbum - PLAY ALBUM')
             self.firstTextbox.insert("0.1", 'playAlbum - PLAY ALBUM' '\n')
 
@@ -466,6 +647,7 @@ class main:
 
     def playBUTT(self):
         try:
+            mus.play()
             lg.info(f'playBUTT - '"NOW PLAYING")
             self.firstTextbox.insert("0.1", 'playBUTT - NOW PLAYING,'  '\n')
 
@@ -479,11 +661,50 @@ class main:
         except Exception as e:
             lg.exception(f'Error in addMultipleSongsBUTT: {e}')
 
+    def openLog(self):
+        try:
+            if os.path.exists(lg_file):
+                try:
+                    subprocess.run(["notepad.exe", lg_file], check=True)
+                except FileNotFoundError:
+                    lg.warning("FAILED TO OPEN LOG")
+            else:
+                lg.warning("FAILED TO OPEN LOG")
+        except Exception as e:
+            lg.exception(f'Error in openLog: {e}')
+
+    def checkMixerStatus(self):
+        try:
+            lg.warning(f'mus.get_init()')
+            self.firstTextbox.insert("0.1", f' MIXER STATUS = {mus.get_init()}.' '\n')
+        except Exception as e:
+            lg.exception(f'Error in checkMixerStatus: {e}')
+
+
+    def activateMixerStatus(self):
+        try:
+            mus.init(frequency=48000, buffer=1024) 
+
+            self.firstTextbox.insert("0.1", f' MIXER STATUS = ACTIVATED.' '\n')
+        except Exception as e:
+            lg.exception(f'Error in activateMixerStatus: {e}')
 
 
 
+    def shutDownMixerStatus(self):
+        try:
+            mus.quit()
 
-            
+            self.firstTextbox.insert("0.1", f' MIXER STATUS = SHUT OFF.' '\n')
+        except Exception as e:
+            lg.exception(f'Error in shutDownMixerStatus: {e}')
+
+                        
+    lg.info('END OF MAIN.PY' '\n' '------------------------------------------------------------------------------------------------------------')
+
+
+
+    
 if __name__ == "__main__":
     root = ctk.CTk()
     app = main(root)
